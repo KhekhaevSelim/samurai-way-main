dd2ca690179f3b3867b08085cb8cfe478b8603bc
KhekhaevSelim <selim16695@mail.ru>
2022-12-28 12:07:11 +0300

    finish 9-th lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import Technologies from "./Technologies";
+import Header from "./Header"
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's do it!
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+
+
+        </div>
+    );
 }
+
+
+
 
 export default App;
